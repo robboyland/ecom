cc7e6b1e043d82481ee394a526a17a5f24fd8475
robboyland <boylandrobert@gmail.com>
2015-11-30 10:44:36 +0000

    add items.show page layout and add to cart button

--- resources/views/store/show.blade.php
+++ resources/views/store/show.blade.php
@@ -2,7 +2,26 @@
 
 @section('content')
 
-    <h1>{{ $item->name }}</h1>
-    <p>{{ $item->description }}</p>
-    <div>{{ $item->cost }}</div>
+    <div class="col-md-10 col-md-offset-1">
+
+        <img class="img-responsive" src="http://placehold.it/700x400" alt="">
+        <h1>{{ $item->name }}</h1>
+        <p>{{ $item->description }}</p>
+        <div>{{ $item->cost }}</div>
+
+        <form action="/cart/store" method="POST">
+
+                {{ csrf_field() }}
+
+                <input name="_method" type="hidden" value="DELETE">
+                <input type="hidden" name="name" value="{{ $item->name }}">
+                <input type="hidden" name="id" value="{{ $item->id }}">
+                <input type="hidden" name="price" value="{{ $item->cost }}">
+
+            <div class="form-group">
+                <input type="submit" value="Add to cart " class="btn btn-primary btn-sm">
+            </div>
+        </form>
+
+    </div>
 @stop
