78fc366cc0500723c397643438da37aab867fa50
robboyland <boylandrobert@gmail.com>
2015-11-30 10:37:31 +0000

    edit store index page: rows, columns & add to cart button

--- resources/views/store/index.blade.php
+++ resources/views/store/index.blade.php
@@ -2,7 +2,34 @@
 
 @section('content')
 
-    @foreach ($items as $item)
-        <a href="/{{ $item->id }}"><h2>{{ $item->name }}</h2></a>
+    @foreach(array_chunk($items->all(), 3) as $row)
+        <div class="row">
+            @foreach($row as $item)
+            <div class="gallery-item col-md-4 portfolio-item">
+                <a href="/{{ $item->id }}">
+                    <img class="img-responsive" src="http://placehold.it/700x400" alt="">
+                </a>
+                <h2>{{ $item->name }}</h2>
+                <div>&pound; {{ $item->price / 100 }}</div>
+            <div>
+
+                <form action="/cart/store" method="POST">
+
+                    {{ csrf_field() }}
+                    <input name="_method" type="hidden" value="DELETE">
+                    <input type="hidden" name="name" value="{{ $item->name }}">
+                    <input type="hidden" name="id" value="{{ $item->id }}">
+                    <input type="hidden" name="price" value="{{ $item->price }}">
+
+                <div class="form-group">
+                    <input type="submit" value="Add to cart " class="btn btn-primary btn-sm">
+                </div>
+
+                </form>
+            </div>
+                <p>{{ $item->description }}</p>
+            </div>
+            @endforeach
+        </div>
     @endforeach
 @stop
