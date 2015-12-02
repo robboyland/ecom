e3345b44ccc1e4709984a8eafc7409b0ccbecfb4
robboyland <boylandrobert@gmail.com>
2015-12-02 14:47:46 +0000

    add Order Model

---
+++ app/Order.php
@@ -0,0 +1,20 @@
+<?php
+
+namespace App;
+
+use Illuminate\Database\Eloquent\Model;
+
+class Order extends Model
+{
+    public function user()
+    {
+        return $this->belongsTo('User');
+    }
+
+    public function orderItems()
+    {
+        return $this->hasMany('\App\OrderItem');
+    }
+
+    protected $fillable = ['user_id', 'total', 'paid', 'charge_id'];
+}
