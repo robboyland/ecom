8acc93878cd3b71d586fa2a0e5510f633e395617
robboyland <boylandrobert@gmail.com>
2015-12-02 14:24:26 +0000

    add migration create_orders_table

---
+++ database/migrations/2015_12_02_111908_create_orders_table.php
@@ -0,0 +1,34 @@
+<?php
+
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class CreateOrdersTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('orders', function (Blueprint $table) {
+            $table->increments('id');
+            $table->integer('user_id')->unsigned();
+            $table->integer('total')->unsigned();
+            $table->string('paid')->nullable();
+            $table->string('charge_id')->nullable();
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::drop('orders');
+    }
+}
