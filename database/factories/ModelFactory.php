4ff902308042b8bffd325b299c8bd2e17e60800b
robboyland <boylandrobert@gmail.com>
2015-12-04 14:05:14 +0000

    add admin role to Users table

--- database/factories/ModelFactory.php
+++ database/factories/ModelFactory.php
@@ -16,6 +16,7 @@ $factory->define(App\User::class, function (Faker\Generator $faker) {
         'name' => $faker->name,
         'email' => $faker->email,
         'password' => bcrypt('password'),
+        'admin' => 0,
         'remember_token' => str_random(10),
     ];
 });
